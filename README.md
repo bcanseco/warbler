5737c966e4f67ebb454df85bb516f719d9f0afa5
bcanseco <b@my.fit.edu>
2017-01-28 23:58:52 -0500

    Add auto-generated ASP.NET MVC boilerplate Changed Contact to Slides and embedded our presentation.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Warbler
+Auto-generated, geo-location based chatrooms for universities.
+
+## Developing
+You will need to install .NET Core and preferably an IDE that supports it.
+
+1. Click [here](https://www.microsoft.com/net/core) and follow the instructions for your OS.
+  * Visual Studio 2015 or later is recommended.
+2. Open a terminal window and navigate to a folder that you want the Warbler code to be.
+3. Run `git clone https://github.com/bcanseco/warbler.git`. 
+
+
+## ToDo
+- [ ] Everything
---
+++ src/Warbler/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warbler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Slides()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}

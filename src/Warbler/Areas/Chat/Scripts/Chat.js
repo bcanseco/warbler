265fa358ca1aee8c4b952114d0927bea5c067ce7
KubusnazKi <whudson2014@my.fit.edu>
2017-04-15 21:03:52 -0400

    added daemonite-material dependency, continued work on Chat JS script

--- src/Warbler/Areas/Chat/Scripts/Chat.js
+++ src/Warbler/Areas/Chat/Scripts/Chat.js
@@ -1,19 +1,56 @@
 ﻿/* global ko */
 var Chat = Chat || {};
-var Data;                                                           //Holds all the actual data that the viewmodel references
+var Data = ko.observable();                                         //Holds all the actual data that the viewmodel references
 
 $(function () {
     Chat.viewModel = new Chat.ViewModel();
     ko.applyBindings(Chat.viewModel);
 })
 
+Chat.Server = function () {
+    var self = this;
+
+    self.hub = $.connection.chatHub;                                //ChatHub.cs
+
+    self.hub.client.onConnection = function (model) {
+        var temp = [];
+        model.Servers
+    }
+
+    self.hub.client.messageReceived = function (messageObject) {
+
+        var server = Data.Servers().forEach(function (server) {
+            return server.serverId = messageObject.serverId;
+        });
+        if (server.length !== 1) {/*fuck shit up*/}                 //TODO: Make a errorHandler utility function
+        
+        var channel = server[0].serverChannels.forEach(function (channel) {
+            return channel.channelId = message.channelId;
+        });
+        if (channel.length !== 1) {/*fuck shit up*/}
+
+        channel[0].channelMessages.push(message);
+
+    }
+
+    $.connection.hub.start()
+        .done(/* do nothing for now */)                             //TODO: Maybe use this to replace onConnection()
+        .fail(function () {
+            console.error("Error connecting to server. Please refresh the page and try again.");
+        });
+
+    $.connection.hub.disconnected(function () {
+        console.error("Lost connection to server. Please refresh the page.");
+    });
+}
+
 Chat.ViewModel = function () {
     var self = this;
-                                                                    //TODO (WH & BC): Get data on servers and channels from signalR hub
-    self.visibleServers = ko.observable();                          //What servers the user has joined and can see                                                               
+
+    self.visibleServers = ko.observable();                          //What servers the user has joined and can see
     self.currentServer = ko.observable();
 
-    self.visibleChannels = ko.computed(function () {
+    self.visibleChannels = ko.computed(function () {                //The channels that are a part of current server
         return self.currentServer().serverChannels();
     });
     self.currentChannel = ko.observable();
@@ -26,7 +63,7 @@ Chat.ViewModel = function () {
         self.currentServer(item);
     }
 
-    self.setChannel = function (item) {
+    self.setChannel = function (item) {                             //Called on click of a channel
         self.currentChannel(item);
     }
 }
@@ -37,7 +74,6 @@ var ServerModel = function (id, name, uni) {
     var organization = uni;
 
     var serverChannels = ko.observableArray([]);
-    //var serverUsers = ko.observableArray([]);                     //TODO (WH & BC): Determine if this is needed
 }
 
 var ChannelModel = function (server, id, name, desc, state) {

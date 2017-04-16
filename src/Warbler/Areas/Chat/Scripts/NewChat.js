9804eb17d35d79ec4c0e79602decaa951cf127b9
KubusnazKi <whudson2014@my.fit.edu>
2017-04-15 23:52:57 -0400

    finished structure of chat JS file, started work on html and styling stuff

--- src/Warbler/Areas/Chat/Scripts/NewChat.js
+++ src/Warbler/Areas/Chat/Scripts/NewChat.js
@@ -1,40 +1,76 @@
 ﻿/* global ko */
-var Chat = Chat || {};
-var Data = ko.observable();                                         //Holds all the actual data that the viewmodel references
+var Chat = Chat || {};                       
 
 $(function () {
+    Chat.server = new Chat.Server();
+    Chat.data = ko.observable();                                    //Holds all the actual data that the viewmodel references
     Chat.viewModel = new Chat.ViewModel();
     ko.applyBindings(Chat.viewModel);
+    ko.applyBindings(Chat.data);
 })
 
 Chat.Server = function () {
     var self = this;
 
-    self.hub = $.connection.chatHub;                                //ChatHub.cs
+    self.hub = $.connection.chatHub;
 
-    self.hub.client.onConnection = function (model) {
+    self.hub.client.onConnection = function (universities) {
         var temp = [];
-        model.Servers
+
+        //Server
+        universities.forEach(function (uni) {
+
+            var _server = new ServerModel(uni.ServerId, uni.ServerName);
+
+            //Channel
+            uni.Server.Channels.forEach(function (channel) {
+
+                var _channel = new ChannelModel(channel.Id, channel.Name, channel.Description, channel.State);
+
+                //Message
+                channel.Messages.forEach(function (message) {
+
+                    var _message = new MessageModel(message.Id, message.UserId, message.SendDate, message.Text);
+
+                    _channel.channelMessages.push(_message);
+
+                })
+
+                _server.serverChannels.push(_channel);
+
+            })
+
+            temp.push(_server);
+
+        })
+
+        Chat.data(temp);
     }
 
     self.hub.client.messageReceived = function (messageObject) {
 
-        var server = Data.Servers().forEach(function (server) {
-            return server.serverId = messageObject.serverId;
+        var server = Chat.data().find(function (server) {
+            return server.serverId() === messageObject.ServerId;
         });
-        if (server.length !== 1) {/*fuck shit up*/}                 //TODO: Make a errorHandler utility function
-        
-        var channel = server[0].serverChannels.forEach(function (channel) {
-            return channel.channelId = message.channelId;
-        });
-        if (channel.length !== 1) {/*fuck shit up*/}
 
-        channel[0].channelMessages.push(message);
+        if (!server) {/*fuck shit up*/}                             //TODO: Make a errorHandler utility function
+        
+        var channel = server.serverChannels().find(function (channel) {
+            return channel.channelId() = message.ChannelId;
+        });
+
+        if (!channel) {/*fuck shit up*/}
+
+        channel.channelMessages.push(new MessageModel(messageObject.MessageId, messageObject.UserId, messageObject.SendDate, messageObject.Text));
+
+    }
+
+    self.hub.client.channelStateUpdate = function () {              //TODO: Fill in method
 
     }
 
     $.connection.hub.start()
-        .done(/* do nothing for now */)                             //TODO: Maybe use this to replace onConnection()
+        .done(/* literally NOTHING xdxdxd */)                       //TODO: Maybe use this to replace onConnection()
         .fail(function () {
             console.error("Error connecting to server. Please refresh the page and try again.");
         });
@@ -47,15 +83,17 @@ Chat.Server = function () {
 Chat.ViewModel = function () {
     var self = this;
 
-    self.visibleServers = ko.observable();                          //What servers the user has joined and can see
+    self.visibleServers = ko.observable(Chat.data());               //What servers the user has joined and can see
     self.currentServer = ko.observable();
+    self.currentChannel = ko.observable();
+
+    self.composedMessage = ko.observable();
 
     self.visibleChannels = ko.computed(function () {                //The channels that are a part of current server
         return self.currentServer().serverChannels();
     });
-    self.currentChannel = ko.observable();
-
-    self.messages = ko.computed(function () {
+    
+    self.visibleMessages = ko.computed(function () {
         return self.currentChannel().channelMessages();
     });
 
@@ -66,18 +104,21 @@ Chat.ViewModel = function () {
     self.setChannel = function (item) {                             //Called on click of a channel
         self.currentChannel(item);
     }
+
+    self.sendMessage = function (item) {
+        Chat.server.hub.server.sendMessage(self.composedMessage());
+        self.composedMessage(null);
+    }
 }
 
-var ServerModel = function (id, name, uni) {
+var ServerModel = function (id, name) {
     var serverId = id;
     var serverName = name;
-    var organization = uni;
 
     var serverChannels = ko.observableArray([]);
 }
 
-var ChannelModel = function (server, id, name, desc, state) {
-    var serverId = server;
+var ChannelModel = function (id, name, desc, state) {
     var channelId = id;
     var channelName = name;
     var channelDesc = desc;
@@ -87,10 +128,8 @@ var ChannelModel = function (server, id, name, desc, state) {
     var channelMessages = ko.observableArray([]);
 }
 
-var MessageModel = function (server, channel, id, user, time, content) {
-    var serverId = server;
-    var channelId = channel;
-    var messageId = id,
+var MessageModel = function (id, user, time, content) {
+    var messageId = id;
     var userId = user;
     var timeSent = time;
     var messageContent = content;

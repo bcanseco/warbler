786fe729415caf0d2281bbada6d0d33b3c22c970
bcanseco <b@my.fit.edu>
2017-08-25 23:07:02 -0400

    Clean up gulp script

--- src/Warbler/gulpfile.js
+++ src/Warbler/gulpfile.js
@@ -1,32 +1,22 @@
 ﻿/// <binding BeforeBuild='default' />
+/* global require */
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    fs = require("fs"),
-    pack = JSON.parse(fs.readFileSync("./package.json")),
-    lib = "./wwwroot/lib";
+const gulp   = require("gulp");
+const rimraf = require("rimraf");
+const fs     = require("fs");
+const pack   = JSON.parse(fs.readFileSync("./package.json"));
+const lib    = "./wwwroot/lib";
 
-/**
- * Removes the ./wwwroot/lib folder and all its contents.
- */
-gulp.task("clean:lib", function (cb) {
-    rimraf(lib, cb);
-});
+gulp.task("cleaning wwwroot", (cb) => rimraf(lib, cb));
 
-/**
- * Moves npm dependencies to ./wwwroot/libs
- */
-gulp.task("move:lib", function () {
-    var libs = Object.keys(pack.dependencies).map(function(dep) {
-        return "node_modules/" + dep + "/**/*.*";
-    });
+gulp.task("moving dependencies to wwwroot", () => {
+    const libs = Object
+        .keys(pack.dependencies)
+        .map((dep) => `node_modules/${dep}/**/*.*`);
 
     return gulp
         .src(libs, { base: "./node_modules" })
         .pipe(gulp.dest(lib));
 });
 
-/**
- * This gulp task will run each of these in order.
- */
-gulp.task("default", gulp.series("clean:lib", "move:lib"));
+gulp.task("default", gulp.series("cleaning wwwroot", "moving dependencies to wwwroot"));

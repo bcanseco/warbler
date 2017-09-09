e5c5a52b0f04ac0fdb89a42e34adad8295b66876
wharris2 <harrisw2014@my.fit.edu>
2017-09-08 23:41:52 -0400

    added unit test for SQLChannel Repository

---
+++ src/Warbler.Tests/Repositories/TestSQLChannelRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Places.Common;
+using GoogleApi.Entities.Places.Search.NearBy.Response;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Warbler.Exceptions;
+using Warbler.Misc;
+using Warbler.Repositories;
+using Warbler.Models;
+using Warbler.Models.Enums;
+using System.Collections.Generic;
+
+namespace Warbler.Tests.Repositories
+{
+    [TestClass]
+    public class TestSQLChannelRepository
+    {
+        private DbContextOptions<WarblerDbContext> Options { get; }
+               = new DbContextOptionsBuilder<WarblerDbContext>()
+                   .UseInMemoryDatabase(nameof(TestSqlUniversityRepository))
+                   .Options;
+
+        [TestMethod]
+        public async Task UpdateAsync_Should_Update_Channel()
+        {
+            using (var context = new WarblerDbContext(Options))
+            {
+                var repo = new SqlChannelRepository(context);
+
+                Channel testChannel = new Channel
+                {
+                    Name = "general",
+                    Description = "",
+                    State = ChannelState.Active,
+                    Type = ChannelType.Normal,
+                    Memberships = new List<Membership>()
+                };
+
+                await repo.UpdateAsync(testChannel);
+            }
+
+            // Use separate context instance to verify correct data was saved to DB
+            using (var context = new WarblerDbContext(Options))
+            {
+                Assert.AreEqual(1, context.Channels.Count());
+                Assert.AreEqual("", context.Channels
+                                         .Where(c => c.Name == "general")
+                                         .FirstOrDefault().Description);
+            }
+        }
+    }
+}

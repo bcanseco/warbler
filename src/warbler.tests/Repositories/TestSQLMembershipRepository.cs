a24070463329b5f6f6fe957ce9c7f27034e9cd5e
wharris2 <harrisw2014@my.fit.edu>
2017-09-10 20:31:24 -0400

    Added additional method to TestSQLMembershipRepository.cs

--- src/warbler.tests/Repositories/TestSQLMembershipRepository.cs
+++ src/warbler.tests/Repositories/TestSQLMembershipRepository.cs
@@ -26,21 +26,19 @@ namespace Warbler.Tests.Repositories
         {
             using (var context = new WarblerDbContext(Options))
             {
-                var repo = new SqlChannelRepository(context);
-                var channel = new Channel()
+                var repo = new SqlUniversityRepository(context);
+                var nearbyResult = new NearByResult
                 {
-                    Name = "general",
-                    Description = "Talk about anything.",
-                    State = ChannelState.Active,
-                    Type = ChannelType.Normal,
-                    Memberships = new List<Membership>()
+                    Name = "Test",
+                    Geometry = new Geometry { Location = new Location(0.0, 0.0) }
                 };
-                await repo.UpdateAsync(channel);
+
+                await repo.CreateAsync(nearbyResult);
             }
         }
 
         [TestMethod]
-        public void AllFor_Should_Return_List_Of_Channels_User_Is_Subscribed()
+        public async Task AllFor_Should_Return_List_Of_Channels_User_Is_Subscribed()
         {
             using (var context = new WarblerDbContext(Options))
             {
@@ -49,17 +47,41 @@ namespace Warbler.Tests.Repositories
                     UserName = "Bob"
                 };
 
-                var channel = context.Channels.FirstOrDefault(c => c.Name == "general");
+                var channels = context.Channels.Include(ch => ch.Memberships);
 
-                channel.Memberships.Add(new Membership { User = user });
+                channels.FirstOrDefault(c => c.Name == "general").Memberships.Add(new Membership { User = user });
             }
 
             using (var context = new WarblerDbContext(Options))
             {
                 var repo = new SqlMembershipRepository(context);
-                Assert.AreEqual(context.Memberships.Include(m => m.User), repo.AllFor(context.Channels
-                                        .FirstOrDefault(c => c.Name == "general")));
+                Assert.AreEqual(1, context.Memberships.Count());
+                Assert.AreEqual(context.Memberships.ToList(), await repo.AllFor(context.Channels
+                                                .FirstOrDefault(c => c.Name == "general")).ToList());
                                         
+            }
+        }
+
+        [TestMethod]
+        public void AllFor_Should_Return_List_Of_Users_Subscribed_To_Channel()
+        {
+            using (var context = new WarblerDbContext(Options))
+            {
+                var user = new User
+                {
+                    UserName = "Bob"
+                };
+
+                var channels = context.Channels.Include(ch => ch.Memberships);
+
+                channels.FirstOrDefault(c => c.Name == "general").Memberships.Add(new Membership { User = user });
+            }
+
+            using (var context = new WarblerDbContext(Options))
+            {
+                var repo = new SqlMembershipRepository(context);
+                Assert.AreEqual(context.Memberships.FirstOrDefault(u => u.User.UserName == "Bob").Channel.Name, repo.AllFor(new User { UserName = "Bob"}));
+
             }
         }
     }

6d90b8f381885f8fc85f74aebaab0cf696b412c9
bcanseco <b@my.fit.edu>
2017-09-15 20:19:36 -0400

    Revert to production so Azure doesn't cry

--- src/Warbler/Startup.cs
+++ src/Warbler/Startup.cs
@@ -56,7 +56,7 @@ namespace Warbler
 
             // Add Entity Framework databases.
             services.AddDbContext<WarblerDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("WarblerDevelopment")));
+                options.UseSqlServer(Configuration.GetConnectionString("WarblerProduction")));
 
             // Set up authentication.
             services.AddIdentity<User, IdentityRole>()

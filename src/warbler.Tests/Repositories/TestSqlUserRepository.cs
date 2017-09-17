256df24b164e8a8ee4a5e6db02bddfde2503a342
wharris2 <harrisw2014@my.fit.edu>
2017-09-16 21:16:53 -0400

    Added a unit test for SqlUserRepository.cs

---
+++ src/warbler.Tests/Repositories/TestSqlUserRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Places.Common;
+using GoogleApi.Entities.Places.Search.NearBy.Response;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warbler.Misc;
+using Warbler.Repositories;
+using Warbler.Models;
+
+namespace Warbler.Tests.Repositories
+{
+    [TestClass]
+    public class TestSqlUserRepository
+    {
+        private DbContextOptions<WarblerDbContext> Options { get; }
+            = new DbContextOptionsBuilder<WarblerDbContext>()
+                .UseInMemoryDatabase(nameof(TestSqlUserRepository))
+                .Options;
+        private User Bob { get; set; }
+        private Channel General { get; set; }
+
+        [TestInitialize]
+        public async Task CreateUniversity()
+        {
+            using (var context = new WarblerDbContext(Options))
+            {
+                var repo = new SqlUniversityRepository(context);
+                var nearbyResult = new NearByResult
+                {
+                    Name = "Test",
+                    Geometry = new Geometry { Location = new Location(0.0, 0.0) }
+                };
+
+                // Create a test university with default channels
+                var testUniversity = await repo.CreateAsync(nearbyResult);
+
+                // Save a reference to one of the default channels and a new user
+                General = testUniversity.Server.Channels.Single(ch => ch.Name == "general");
+                Bob = new User { UserName = "Bob" };
+
+                // Add a test user to it
+                General.Memberships.Add(new Membership { User = Bob });
+
+                // Save it to the in-memory database for use in tests
+                await repo.SaveAsync();
+            }
+        }
+
+        [TestMethod]
+        public async Task IsNewAsync_should_return_false_if_user_already_existed()
+        {
+            using (var context = new WarblerDbContext(Options))
+            {
+                var repo = new SqlUserRepository(context);
+                Assert.AreEqual(1, context.Memberships.Count());
+                Assert.IsTrue(!(await repo.IsNewAsync(Bob)));
+            }
+        }
+
+        [TestMethod]
+        public async Task IsNewAsync_should_return_True_If_User_Was_Recently_Created()
+        {
+            using (var context = new WarblerDbContext(Options))
+            {
+                var repo = new SqlUserRepository(context);
+                User Jimmy = new User { UserName = "Jimmy" };
+                Assert.IsTrue(await repo.IsNewAsync(Jimmy));
+            }
+        }
+    }
+}

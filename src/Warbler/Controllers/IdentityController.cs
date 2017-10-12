6e0d855aca772769cae33c72718f256f10c434c2
Winter <yxiaohan2014@gmail.com>
2017-10-12 00:23:08 -0500

    Add identity controller

---
+++ src/Warbler/Controllers/IdentityController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Warbler.Interfaces;
+using Warbler.Models;
+using Warbler.Models.AccountViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace Warbler.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from a in User.Claims select new { a.Type, a.Value });
+        }
+    }
+}

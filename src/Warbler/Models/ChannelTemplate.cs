ad876105b234a6d8818305f1d3cf85f2bbdc1ddc
bcanseco <b@my.fit.edu>
2017-11-15 00:29:02 -0500

    Add ctor to template model

--- src/Warbler/Models/ChannelTemplate.cs
+++ src/Warbler/Models/ChannelTemplate.cs
@@ -2,6 +2,14 @@
 {
     public class ChannelTemplate
     {
+        /// <summary>
+        ///   Parameterless ctor required by EF.
+        /// </summary>
+        public ChannelTemplate() {}
+
+        public ChannelTemplate(string name, string description)
+            => (Name, Description) = (name, description);
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }

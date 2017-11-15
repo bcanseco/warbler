cd11764bc1f4f90d1e232298c1954066c5ffd07f
bcanseco <b@my.fit.edu>
2017-11-14 23:47:08 -0500

    Use lowercase string best practices

--- src/Warbler/Models/ChannelTemplate.cs
+++ src/Warbler/Models/ChannelTemplate.cs
@@ -8,7 +8,7 @@ namespace Warbler.Models
     public class ChannelTemplate
     {
         public int Id { get; set; }
-        public String Name { get; set; }
-        public String Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }

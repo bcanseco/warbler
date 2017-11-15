47f5afd87b7954d59687c1ec906257e8413244f8
bcanseco <b@my.fit.edu>
2017-11-15 00:29:41 -0500

    Change service to use repo's new methods

--- src/Warbler/Services/ChannelTemplateService.cs
+++ src/Warbler/Services/ChannelTemplateService.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Warbler.Interfaces;
 using Warbler.Models;
 
 namespace Warbler.Services
 {
+    /// <summary>
+    ///   The business logic layer for manipulation of channel templates.
+    /// </summary>
     public class ChannelTemplateService
     {
         private IChannelTemplateRepository Repository { get; }
@@ -14,10 +18,19 @@ namespace Warbler.Services
         }
 
         /// <summary>
-        ///   Updates a channel's properties (like messages and users).
+        ///   Loads the database up with a default set of channel templates.
         /// </summary>
-        /// <param name="channel">The channel to update.</param>
-        public async Task UpdateAsync(ChannelTemplate channelTemplate)
-            => await Repository.UpdateAsync(channelTemplate);
+        public async Task CreateDefaultTemplatesAsync()
+        {
+            await Repository.CreateAsync(new ChannelTemplate("general", "Talk about anything."));
+            await Repository.CreateAsync(new ChannelTemplate("biology", "Biology discussion."));
+            await Repository.CreateAsync(new ChannelTemplate("compsci", "Computer science news and help."));
+            await Repository.CreateAsync(new ChannelTemplate("math", "2 + 2 = 4"));
+            await Repository.CreateAsync(new ChannelTemplate("psych", "Discuss the psychology curriculum."));
+            await Repository.CreateAsync(new ChannelTemplate("physics", "What goes up must come down."));
+            await Repository.CreateAsync(new ChannelTemplate("law", "Lady liberty's channel"), true);
+        }
+
+        public async Task<List<ChannelTemplate>> GetAsync() => await Repository.GetAsync();
     }
 }

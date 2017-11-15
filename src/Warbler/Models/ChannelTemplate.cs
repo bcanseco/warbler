916540519381a0c6df0a328f377bb6d186373419
bcanseco <b@my.fit.edu>
2017-11-14 23:47:24 -0500

    Remove unused channel template imports

--- src/Warbler/Models/ChannelTemplate.cs
+++ src/Warbler/Models/ChannelTemplate.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Warbler.Models
+﻿namespace Warbler.Models
 {
     public class ChannelTemplate
     {
